cd867eff933c7d2ef68cb2ac384ab807eab97c24
m0cchi <boom.boom.planet@gmail.com>
2016-06-19 21:23:04 +0900

    modify getFileName

--- src/main/java/shaman/util/Util.java
+++ src/main/java/shaman/util/Util.java
@@ -3,7 +3,7 @@ package shaman.util;
 import java.util.Arrays;
 
 public class Util {
-	final public static String[] FORMATS = { "jpg", "png" , "jpeg", "gif"};
+	final public static String[] FORMATS = { "jpg", "png", "jpeg", "gif" };
 
 	public static boolean isSupportedFormats(String path) {
 		return Arrays.asList(FORMATS).contains(getExt(path.toLowerCase()));
@@ -24,13 +24,13 @@ public class Util {
 		if (point < 0) {
 			point = path.lastIndexOf("\\");
 		}
-		
+
 		path = path.substring(point + 1);
 		point = path.indexOf(".");
 		if (point != -1) {
 			return path.substring(0, point);
 		}
-		return null;
+		return path;
 	}
 
 	public static boolean isEmpty(String str) {
